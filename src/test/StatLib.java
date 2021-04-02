42eed48202afb476416fa8bf97d786218287a51f
Aviya Shirian <aviya.shirian@gmail.com>
2021-04-02 21:40:12 +0300

    implement dev

--- src/test/StatLib.java
+++ src/test/StatLib.java
@@ -62,12 +62,12 @@ public class StatLib {
 
 	// returns the deviation between point p and the line equation of the points
 	public static float dev(Point p,Point[] points){
-		return 0;
+		return StatLib.dev(p, StatLib.linear_reg(points));
 	}
 
 	// returns the deviation between point p and the line
 	public static float dev(Point p,Line l){
-		return 0;
+		return Math.abs(l.f(p.x) - p.y);
 	}
 	
 }
