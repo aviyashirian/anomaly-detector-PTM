6854bbe1ba47d527b288c244df5d3d48592e3a40
Aviya Shirian <aviya.shirian@gmail.com>
2021-04-03 13:56:44 +0300

    change implementation of TimeSeries

--- src/test/TimeSeries.java
+++ src/test/TimeSeries.java
@@ -4,38 +4,59 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class TimeSeries {
-	public String[] field_names;
-	public List<float[]> values;
+	private List<String> fields;
+	private Map<String, List<Float>> time_steps;
 	
 	public TimeSeries(String csvFileName) {
+		this.fields = new ArrayList<>();
+		this.time_steps = new HashMap<>();
+
 		try (BufferedReader csvReader = new BufferedReader(new FileReader(csvFileName))) {
 
 			String row = csvReader.readLine();
 			if(row != null) {
-				this.field_names = row.split(",");
-				row = csvReader.readLine();
-			}
-
-			this.values = new ArrayList<float[]>();
-			while (row != null) {
-				String[] data = row.split(",");
-				float[] row_values = new float[data.length];
-
-				for (int i = 0; i < data.length; i++) {
-					row_values[i] = Float.parseFloat(data[i]);
+				for (String field : row.split(",")) {
+					this.fields.add(field);
+					this.time_steps.put(field, new ArrayList<Float>());
 				}
-
-				this.values.add(row_values);
-
-				row = csvReader.readLine();
 			}
-			csvReader.close();
+
+			while ((row = csvReader.readLine()) != null) {
+				String[] row_values = row.split(",");
+				for (int i = 0; i < row_values.length; i++) {
+					this.time_steps.get(this.fields.get(i)).add(Float.parseFloat(row_values[i]));
+				}
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+	}
+
+	public List<String> getFieldNames() {
+		return this.fields;
+	}
+
+	public float[] getColumn(int index) {
+		List<Float> list = this.time_steps.get(this.fields.get(index));
+		float[] arr = new float[list.size()];
+		for(int i = 0; i < list.size(); i++) arr[i] = list.get(i);
+
+		return arr;
+	}
+
+	public Point[] getPoints(int feature1, int feature2) {
+		float[] x = this.getColumn(feature1);
+		float[] y = this.getColumn(feature2);
+		Point[] points = new Point[x.length];
+		for (int i = 0; i < points.length; i++) {
+			points[i] = new Point(x[i], y[i]);
+		}
+
+		return points;
 	}
 }
