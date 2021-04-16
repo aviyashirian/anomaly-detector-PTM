2432862395d3207b8f7da0a16b04b5ae9208a2fb
Aviya Shirian <aviya.shirian@gmail.com>
2021-04-16 21:34:22 +0300

    server

--- src/test/AnomalyDetectionHandler.java
+++ src/test/AnomalyDetectionHandler.java
@@ -1,33 +1,58 @@
 package test;
 
-
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Scanner;
 import test.Commands.DefaultIO;
 import test.Server.ClientHandler;
 
 public class AnomalyDetectionHandler implements ClientHandler{
 
 	public class SocketIO implements DefaultIO{
+		private Scanner in;
+		private PrintWriter out;
 
+		public SocketIO(InputStream clientInput, OutputStream clientOutput) {
+			this.in = new Scanner(clientInput);
+			this.out = new PrintWriter(clientOutput);			
+		}
+		
 		@Override
 		public String readText() {
-			return null;
+			return this.in.nextLine();
 		}
 
 		@Override
 		public void write(String text) {
-			
+			this.out.print(text);
+			this.out.flush();
 		}
 
 		@Override
 		public float readVal() {
-			return 0;
+			return this.in.nextFloat();
 		}
 
 		@Override
 		public void write(float val) {
-			
+			this.out.print(val);
+			this.out.flush();
 		}
 
+		public void close() {
+			this.in.close();
+			this.out.close();
+		}
+
+	}
+
+	@Override
+	public void handle(InputStream input, OutputStream output) {
+		SocketIO sio = new SocketIO(input, output);
+		CLI cli = new CLI(sio);
+		cli.start();
+		sio.close();
 	}
 
 
--- src/test/Server.java
+++ src/test/Server.java
@@ -1,9 +1,16 @@
 package test;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+
 public class Server {
 
 	public interface ClientHandler{
-		// define...
+		public void handle(InputStream inFromClient, OutputStream outToClient);
 	}
 
 	volatile boolean stop;
@@ -13,7 +20,18 @@ public class Server {
 	
 	
 	private void startServer(int port, ClientHandler ch) {
-		// implement here the server...
+		try {
+			ServerSocket server = new ServerSocket(port);
+			while (!stop) {
+				Socket client = server.accept();
+				ch.handle(client.getInputStream(), client.getOutputStream());
+				client.close();
+			}
+			server.close();
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	// runs the server in its own thread
